cb3849c37893d9fcd4a977afcf7a5f4f68c82c07
ruslan222 <kaminruslan@yandex.ru>
2025-12-01 17:09:15 +0400

    .

--- sandbox/src/main/java/Geometry.java
+++ sandbox/src/main/java/Geometry.java
@@ -11,6 +11,7 @@ public class Geometry {
 
     private static void printRectangleArea(double a, double b) {
         System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + "  = " +  rectangleArea(a, b));
+        
 
     }
 
